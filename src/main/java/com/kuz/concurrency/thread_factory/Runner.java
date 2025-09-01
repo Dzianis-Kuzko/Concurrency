04a4c411e2b7a5428c2960e81767289388155d87
Dzianis Kuzko <dkuzko@tut.by>
2025-09-01 11:15:10 +0300

    threadFactory

---
+++ src/main/java/com/kuz/concurrency/thread_factory/Runner.java
@@ -0,0 +1,53 @@
+package com.kuz.concurrency.thread_factory;
+
+import java.util.concurrent.ThreadFactory;
+
+public class Runner {
+    private static final String MESSAGE_EXCEPTION_TEMPLATE = "Exception was thrown with message '%s' in thread '%s'.\n";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread.UncaughtExceptionHandler uncaughtExceptionHandler = (thread, exception)
+                -> {
+            System.out.printf(MESSAGE_EXCEPTION_TEMPLATE, exception.getMessage(), thread.getName());
+        };
+
+        ThreadFactory threadFactory = new DaemonThreadWithUncaughtExceptionHandlerFactory(uncaughtExceptionHandler);
+
+        Thread firstThread = threadFactory.newThread(new Task());
+        firstThread.start();
+
+        Thread secondThread = threadFactory.newThread(new Task());
+        secondThread.start();
+
+        firstThread.join();
+        secondThread.join();
+
+    }
+
+    private static final class Task implements Runnable {
+        private static final String EXCEPTION_MESSAGE = "I am exception";
+
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().isDaemon());
+            throw new RuntimeException(EXCEPTION_MESSAGE);
+        }
+    }
+
+    private static final class DaemonThreadWithUncaughtExceptionHandlerFactory implements ThreadFactory {
+        private final Thread.UncaughtExceptionHandler uncaughtExceptionHandler;
+
+        private DaemonThreadWithUncaughtExceptionHandlerFactory(Thread.UncaughtExceptionHandler uncaughtExceptionHandler) {
+            this.uncaughtExceptionHandler = uncaughtExceptionHandler;
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r);
+            thread.setUncaughtExceptionHandler(uncaughtExceptionHandler);
+            thread.setDaemon(true);
+            return thread;
+        }
+    }
+}
