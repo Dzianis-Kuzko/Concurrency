87b80c99b98b51530be71da9af3feaadc5593a58
Dzianis Kuzko <dkuzko@tut.by>
2025-09-09 22:33:28 +0300

    race conditions

---
+++ src/main/java/com/kuz/concurrency/race_conditions/Runner.java
@@ -0,0 +1,33 @@
+package com.kuz.concurrency.race_conditions;
+
+import java.util.stream.IntStream;
+
+public class Runner {
+    private static int counter = 0;
+    private static final int INCREMENT_AMOUNT_FIRST_THREAD = 500;
+    private static final int INCREMENT_AMOUNT_SECOND_THREAD = 600;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread firstThread = createIncrementingCounterThread(INCREMENT_AMOUNT_FIRST_THREAD);
+        Thread secondThread = createIncrementingCounterThread(INCREMENT_AMOUNT_SECOND_THREAD);
+
+        firstThread.start();
+        secondThread.start();
+
+        firstThread.join();
+        secondThread.join();
+
+        System.out.println(counter);
+
+
+    }
+
+    private static Thread createIncrementingCounterThread(int incrementAmount) {
+        return new Thread(() -> IntStream.range(0, incrementAmount)
+                .forEach(i -> incrementCounter()));
+    }
+
+    private static synchronized void incrementCounter(){
+        counter++;
+    }
+}
